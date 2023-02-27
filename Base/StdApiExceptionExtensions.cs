4362d4515c0f8721432b575a9fb9b0683c41efd3
@leolant <leandrooliveiralinoara@correio.itau.com.br>
2023-02-27 00:58:24 -0300

    remocao do plano IErrorToResultConverter

--- Base/StdApiExceptionExtensions.cs
+++ Base/StdApiExceptionExtensions.cs
@@ -3,10 +3,5 @@ using System.Text.Json;
 
 namespace StandardApiTools {
     public static class StdApiExceptionExtensions {
-        public static E SetCustomResultType<E, T>(this E ex, T resultObject)
-        where E : StdApiExceptionBase where T : IErrorToResultConverter<E> {
-            resultObject.SetSourceException(ex);
-            return ex;
-        }
     }
 }
--- Base/StdApiInterfaces.cs
+++ Base/StdApiInterfaces.cs
@@ -13,44 +13,4 @@
     public interface IAddInfo {
         public IAddInfo AddInfo(string key, object value);
     }
-
-
-
-    public interface IErrorToResultConverter<T> : IProduceStdApiErrorResult where T : StdApiExceptionBase {
-        void SetSourceException(T excpetion);
-    }
-
-
-
-    //public abstract class StdApiCustomErrorResult<T>: IProduceStdApiErrorResult where T : StdApiExceptionBase {
-    //    protected T exception;
-    //    public void SetSourceException(T exception) {
-    //        this.exception = exception;
-    //    }
-    //    public abstract StdApiErrorResult ToResult();
-    //    StdApiResult IProduceStdApiResult.ToResult() => ToResult();
-    //}
-
-
-
-    public abstract class StdApiCustomErrorResult<T>: IProduceStdApiErrorResult where T : StdApiExceptionBase {
-        protected StdApiCustomErrorResult(T exception) => this.exception = exception;
-        private T exception;
-        protected abstract object CompileResultContent(object exceptionContent);
-        public StdApiErrorResult ToResult() => new StdApiErrorResult(exception.StatusCode, exception.Message, CompileResultContent(exception.Content), exception.Info, true);
-        StdApiResult IProduceStdApiResult.ToResult() => ToResult();
-    }
-
-
-
-
-    //public class ObterConteudoVinculadoErrorResult404: IErrorToResultConverter<StdApiWebException> {
-    //    public void Receive(StdApiWebException exception) {
-    //        throw new System.NotImplementedException();
-    //    }
-
-    //    public StdApiResult ToResult() {
-    //        throw new System.NotImplementedException();
-    //    }
-    //}
 }
