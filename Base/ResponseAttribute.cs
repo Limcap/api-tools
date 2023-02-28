e6c6a38aca3d8f17b654825694073c96fba6d53d
@leolant <leandrooliveiralinoara@correio.itau.com.br>
2023-02-28 18:02:05 -0300

    Nova feature ResponseAttribute

---
+++ Base/ResponseAttribute.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace StandardApiTools {
+    public class ResponseAttribute: ProducesResponseTypeAttribute {
+        public ResponseAttribute(int statusCode, Type type) : base(type, statusCode) {}
+    }
+}
